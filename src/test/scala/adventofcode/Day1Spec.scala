248e9f8b721f3b8ee15cbed68689edf567f03886
Adam Pridmore <320922+adampridmore@users.noreply.github.com>
2023-12-03 21:38:27 +0000

    Day 1 - part 1

--- src/test/scala/adventofcode/Day1Spec.scala
+++ src/test/scala/adventofcode/Day1Spec.scala
@@ -6,91 +6,47 @@ import org.scalatest.matchers.should.Matchers
 class Day1Spec extends AnyWordSpec with Matchers with AdventOfCodeHelper {
   def inputFilename() = "day1/input.txt"
 
-  private def textToInts(lines: String) : Seq[Option[Int]] = {
-    lines
-      .split(System.lineSeparator())
-      .map(line => line.trim)
-      .map({
-          case "" => None
-          case line => Some(line.toInt)
-        })
-      .toList
+  def puzzleText() : String = {
+    data
   }
 
-  val dataInts : Seq[Option[Int]] = textToInts(data)
-
   val exampleText : String = """
-1000
-2000
-3000
-
-4000
-
-5000
-6000
-
-7000
-8000
-9000
-
-10000
+1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet
 """
 
-  val exampleInts : Seq[Option[Int]] = textToInts(exampleText)
-
   def assertAndPrint[T](message: String, answer: T, expected: T) = {
+    println(s"Puzzle: $message: Answer: $answer")
     answer shouldBe expected
   }
   
-  def splitIntoElves(textInput : String ) : Seq[Int] = {
-    
-    val lines = textToInts(textInput)
-
-    val elves = Array.newBuilder[Int]
-    
-    var caloriesTotal = 0
-    for (line <- lines){
-      line match {
-        case None => {
-          elves += caloriesTotal
-          caloriesTotal = 0
-        }
-        case Some(calories) => caloriesTotal += calories
-      }
-    }
-    elves += caloriesTotal
-
-    elves.result()
-  }
-
   def solveA(textInput : String ) : Int = {
-    val elves = splitIntoElves(textInput)
-    elves.max
-  }
+    val lines = textInput.split(System.lineSeparator());
 
-  def solveB(textInput : String ) : Int = {
-    splitIntoElves(textInput)
-      .sortBy(identity)
-      .reverse
-      .take(3)
-      .sum
+    def processLine(line: String) : Int = {
+      var digitCharacters = 
+          line.toCharArray()
+          .filter(Character.isDigit(_))
+
+      Integer.parseInt(digitCharacters.head.toString() + digitCharacters.last.toString())
+    }
+
+    var lineValues = lines
+      .filter(!_.isBlank())
+      .map(processLine(_))
+
+    lineValues.sum
   }
   
   "Day 1" should {
     "Part 1 Example" in {
-      assertAndPrint("Part 1 example", solveA(exampleText), 24000)
+      assertAndPrint("Part 1 example", solveA(exampleText), 142)
     }
 
-    "Part 1" in {
-       assertAndPrint("Part 1", solveA(data), 69795)
-     }
-
-    "Part 2 Example" in {
-      assertAndPrint("Part 1 example", solveB(exampleText), 45000)
-     }
-
-    "Part 2" in {
-      assertAndPrint("Part 1", solveB(data), 208437)
-     }
+    "Part 1 puzzle" in {
+      assertAndPrint("Part 1 puzzle", solveA(puzzleText()), 54338)
+    }
   }
 }
